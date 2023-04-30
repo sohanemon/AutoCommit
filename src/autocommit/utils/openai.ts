2efc8042729653688ea29a041cbaef1921bdb724
sohanemon <m.sohanemon@gmail.com>
2023-04-30 11:38:04 +0600

    refactor(openai.ts): remove commented out code and update createChatCompletion options feat(openai.ts): add support for frequency_penalty and presence_penalty options in createChatCompletion

--- src/autocommit/utils/openai.ts
+++ src/autocommit/utils/openai.ts
@@ -10,33 +10,54 @@ import { generateCommitMessageChatCompletionPrompt } from './completion';
 import { trimNewLines } from './text';
 
 export const generateCommitMessage = async (
-    apiKey: string,
-    diff: string,
-    delimeter?: string,
+  apiKey: string,
+  diff: string,
+  delimeter?: string
 ) => {
-    const messages = generateCommitMessageChatCompletionPrompt(diff);
+  const messages = generateCommitMessageChatCompletionPrompt(diff);
 
-    const openAI = new OpenAIApi(new Configuration({
-        apiKey: apiKey
-    }));
+  const openAI = new OpenAIApi(
+    new Configuration({
+      apiKey: apiKey,
+    })
+  );
 
-    const { data } = await openAI.createChatCompletion(
-        {
-            model: 'gpt-3.5-turbo',
-            messages: messages,
-            temperature: 0,
-            ['top_p']: 0.1,
-            ['max_tokens']: 196
-        }
-    );
+  // async function createChatCompletion(diff: string): Promise<string> {
+  //   const prompt = `Generate a commit message from the following diff:\n\n${diff}\n\nCommit message:`;
+  //   const response = await openAI.Completion.create({
+  //     engine: 'text-davinci-002',
+  //     prompt,
+  //     temperature: 0.5,
+  //     max_tokens: 60,
+  //     top_p: 0.9,
+  //     frequency_penalty: 0,
+  //     presence_penalty: 0,
+  //   });
 
-    const message = data?.choices[0].message;
-    const commitMessage = message?.content;
+  //   // Parse response and extract generated commit message
+  //   const message = _.trim(response.choices[0].text);
+  //   const cleanedMessage = message.replace(/[^\x00-\x7F]+/g, ''); // Remove non-ASCII characters
 
-    if (commitMessage) {
-        const alignedCommitMessage = trimNewLines(commitMessage, delimeter);
-        return alignedCommitMessage;
-    }
+  //   return cleanedMessage;
+  // }
 
-    return;
+  const { data } = await openAI.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: messages,
+    temperature: 0,
+    ['top_p']: 0.9,
+    ['max_tokens']: 60,
+    ['frequency_penalty']: 0,
+    ['presence_penalty']: 0,
+  });
+
+  const message = data?.choices[0].message;
+  const commitMessage = message?.content.replace(/[^\x00-\x7F]+/g, '');
+
+  if (commitMessage) {
+    const alignedCommitMessage = trimNewLines(commitMessage, delimeter);
+    return alignedCommitMessage;
+  }
+
+  return;
 };
